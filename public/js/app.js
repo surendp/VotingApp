e0966b423eaa17f20768334b03aed32092effc44
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-05-04 18:38:24 +0300

    Create Product component

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,33 @@ class ProductList extends React.Component{
   render(){
     return(
       <div className= 'ui unstackable items'>
-        Hello, friend! I am a basic React component.
+        <Product/>
+      </div>
+    )
+  }
+}
+
+// Product component -> A child component for the ProductList Component
+class Product extends React.Component{
+  render(){
+    return(
+      <div className='item'>
+        <div className='image'>
+          <img src='images/products/image-aqua.png'/>
+        </div>
+        <div className='middle aligned content'>
+          <div className='description'>
+            <a>Fort Knight</a>
+            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className = 'ui avatar image'
+              src='images/avatars/daniel.jpg'
+            />
+          </div>
+        </div>
       </div>
     )
   }
@@ -17,6 +43,7 @@ ReactDOM.render(
   <ProductList/>,
   document.getElementById('content')
 );
+
 /*
 // ProductList component using JavaScript
 const ProductList1 = createReactClass({
